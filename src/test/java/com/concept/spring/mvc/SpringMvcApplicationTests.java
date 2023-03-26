8826cc381a8e8b9ad5ed3f60c48d00dc470f97b2
sudhaScode <sunriser7995@gmail.com>
2023-03-26 13:34:22 +0530

    initial commit

---
+++ src/test/java/com/concept/spring/mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concept.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
